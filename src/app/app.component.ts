38d31bcc0861f2f8b4888149b5ea04bfe4c6a712
Carolina Candelaria Alvarez Martin <carolina.alvarezm@eviden.com>
2024-04-26 01:11:57 +0100

    Ejemplo Calculadora completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Calculadora';
+  numero1:number=0;
+  numero2:number=0;
+  resultado:number=0;
+
+  sumar():void{
+    this.resultado=this.numero1+this.numero2;
+  }
+  restar():void{
+    this.resultado=this.numero1-this.numero2;
+  }
+  multiplicar():void{
+    this.resultado=this.numero1*this.numero2;
+  }
+  dividir():void{
+    if(this.numero2!=0){
+      this.resultado=this.numero1/this.numero2;
+    }
+    else{
+      alert("No podemos dividir por 0");
+      this.numero1=0;
+      this.numero2=0;
+    }
+    
+  }
 }
